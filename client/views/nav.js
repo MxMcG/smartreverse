983239acba4fcea52a7f521d9259f4ee20428f6a
MxMcG <mmcgeevibes@gmail.com>
2016-01-25 08:03:08 -0800

    navbar logo for iphone 5

--- client/views/nav.js
+++ client/views/nav.js
@@ -7,9 +7,11 @@
       // set distance user needs to scroll before we fadeIn navbar
 
       if ($(this).scrollTop() > 550) {
-        $('.navbar').css('background', 'lightgray')
+        $('.navbar').css('background', '#3a4349')
+        $('.navbar-inverse .navbar-nav > li > a ').css('color', 'white')
       } else if ($(this).scrollTop() < 550) {
         $('.navbar').css('background', 'transparent');
+        $('.navbar-inverse .navbar-nav > li > a ').css('color', '#3a4349')
       }
 
     });
