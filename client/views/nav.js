baaa147d2282fcb4b51cfa6b8fe7d66503818e3c
MxMcG <mmcgeevibes@gmail.com>
2016-01-26 11:42:48 -0800

    add telephone

--- client/views/nav.js
+++ client/views/nav.js
@@ -8,10 +8,12 @@
 
       if ($(this).scrollTop() > 550) {
         $('.navbar').css('background', '#3a4349')
-        $('.navbar-inverse .navbar-toggle .icon-bar').css('background-color', 'white')
+        $('.navbar-inverse .navbar-toggle .icon-bar').css('background-color', 'white');
+        $('.navbar a').css('color', 'white');
       } else if ($(this).scrollTop() < 550) {
         $('.navbar').css('background', 'transparent');
-        $('.navbar-inverse .navbar-toggle .icon-bar').css('background-color', '#3a4349')
+        $('.navbar-inverse .navbar-toggle .icon-bar').css('background-color', '#3a4349');
+        $('.navbar a').css('color', '#3a4349');
       }
 
     });
