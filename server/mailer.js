f65d306f71a1e24bb30a33bf1defa0d7c6ae4775
MxMcG <mmcgeevibes@gmail.com>
2016-01-27 17:13:34 -0800

    mailer

--- server/mailer.js
+++ server/mailer.js
@@ -1,3 +1,5 @@
 Meteor.startup( function() {
 
+  // process.env.MAIL_URL = 'smtp://postmaster%40sandbox853366c7d48d4cbe8bcd78d30083fc9a.mailgun.org:7fb32726bee779c57ac8f3f297d7f715@smtp.mailgun.org:587';
+
 });
